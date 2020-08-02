b767bf134bde861d0e95a782b2a2e5099a1affa5
Tomi Nurhidayat <60332891+tominh@users.noreply.github.com>
2020-08-02 23:20:58 +0700

    Membuat Jasa Ketik Online

--- javascript/script.js
+++ javascript/script.js
@@ -8,7 +8,7 @@ function scrollFunction() {
     if (document.body.scrollTop > 60 || document.documentElement.scrollTop > 60) {
         document.getElementById("navbar").style.top = "0";
     } else {
-        document.getElementById("navbar").style.top = "-300px";
+        document.getElementById("navbar").style.top = "-500px";
     }
 }
 
